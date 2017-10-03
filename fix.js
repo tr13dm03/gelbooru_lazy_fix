79b72f9c3eb51d431e0ec95c643be3b39547e403
tr13dm03 <38781134854882673224@netcourrier.com>
2017-10-03 18:29:41 -0400

    Fix the script and shorten it
    The name used for fetching the informations doesn't exist anymore in the
    dataset. This was fixed, along with shortening the script and converting
    it to ES6.

--- fix.js
+++ fix.js
@@ -1,16 +1,15 @@
 // ==UserScript==
 // @name Gelbooru Lazyload Fix
-// @version 1.0.1
+// @version 1.0.2
 // @namespace atribecalledkwest
 // @description This script fixes the Gelbooru thumbnails if the thumbnail lazyloader doesn't run (typically due to an Adblocker)
 // @include https://gelbooru.com/*
 // @include http://gelbooru.com/*
+// @grant None
 // ==/UserScript==
 
 (function() {
-    var thumbs = document.querySelectorAll("img.lazyload[data-src]");
-    for(var i = 0; i < thumbs.length; i++) {
-        var img = thumbs[i];
-        img.src = img.dataset.src;
-    }
-})();
+    var thumbs = document.querySelectorAll("img.lazyload[data-original]");
+    for (let img of thumbs) {
+        img.src = img.dataset["original"];
+    } })();
